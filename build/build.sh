3b261ba3d07db8c687be3add27ad26063462ff36
colinpaint <88828347+colinpaint@users.noreply.github.com>
2024-05-11 10:05:04 +0100

    make builds work

---
+++ build/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+fpc -gl -o"../pascal2" -Fu../pascal -Fu../common -Fu../mc68000  ../pascal/main
--- mc68000/build_pascal.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-fpc -gl -opas2m68k -FE. -Fu../pascal -Fu../common -Fu../mc68000  ../pascal/main
